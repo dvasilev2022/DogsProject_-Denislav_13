1408ac84eff2499c85e00221851610406326b1ae
dvasilev2022 <m.kirilov2020@pmg-pernik.com>
2026-05-11 09:17:29 +0300

    Добавени файлове от ученик

---
+++ DogsApp.Core/Contracts/IDogService.cs
@@ -0,0 +1,23 @@
+﻿using DogsApp.Ifrastructure.Data.Domain;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogsApp.Core.Contracts
+{
+    public interface IDogService
+    {
+        bool Create(string name, int age, int breedId, string picture);
+        bool UpdateDog(int dogId,string name, int age,int breedId,string picture);
+
+        List<Dog>GetDogs();
+
+        Dog GetDogById(int dogId);
+        bool RemoveById(int dogId);
+
+        List<Dog> GetDogs(string searchStringBreed, string searchStringName);
+    }
+}
